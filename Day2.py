c549070636c7503e80a74cb726c76576c8173d92
Pratibha Agrawal <agrawalpratibha63@gmail.com>
2026-03-12 23:41:20 +0530

    Day 2 Variables and Data Types

---
+++ Day2.py
@@ -0,0 +1,19 @@
+#=======================================================================
+            #Day2 Variables,data types and operators
+#=======================================================================
+
+#Practice 1:
+a=3
+b=3.0
+c="3"
+d=True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+#Practice2:
+num=int(input("enter a number:"))
+new_num=num+10
+final_str=str(new_num)
+print(final_str)
