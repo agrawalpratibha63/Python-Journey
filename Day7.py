74cfa856bb9edc29e1127cd3c1f64bd5fd5456cb
Pratibha Agrawal <agrawalpratibha63@gmail.com>
2026-03-19 19:08:41 +0530

    Day7 pattern practice

--- Day7.py
+++ Day7.py
@@ -6,73 +6,73 @@
 # **
 # ***
 # ****
-# r=int(input("enter no of rows:"))
-# for i in range(r):
-#     for j in range(i+1):
-#         print("*",end=" ")
-#     print()
+r=int(input("enter no of rows:"))
+for i in range(r):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
 # ****
 # ***
 # **
 # *
-# n=int(input("enter no of rows:"))
-# for i in range(n):
-#     for j in range(n,0,-1):
-#         print("*",end=" ")
-#     print()
-#     n=n-1
+n=int(input("enter no of rows:"))
+for i in range(n):
+    for j in range(n,0,-1):
+        print("*",end=" ")
+    print()
+    n=n-1
 # 1
 # 12
 # 123
 # 1234
-# r=int(input("enter no of rows:"))
-# for i in range(1,r+1):
-#     for j in range(1,i+1):
-#         print(j,end=" ")
-#     print()
+r=int(input("enter no of rows:"))
+for i in range(1,r+1):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
 # A
 # BB
 # CCC
 # DDDD
-# n=int(input("enter no of rows:"))
-# a=65
-# for i in range(1,n+1):
-#     for j in range(1,i+1):
-#         ch=chr(a)
-#         print(ch,end=" ")
-#     print()
-#     a=a+1
+n=int(input("enter no of rows:"))
+a=65
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        ch=chr(a)
+        print(ch,end=" ")
+    print()
+    a=a+1
 # A
 # AB
 # ABC
 # ABCD
-# n=int(input("enter no of rows:"))
-# for i in range(1,n+1):
-#     a=65
-#     for j in range(1,i+1):
-#         ch=chr(a)
-#         print(ch,end=" ")
-#         a=a+1
-#     print()
+n=int(input("enter no of rows:"))
+for i in range(1,n+1):
+    a=65
+    for j in range(1,i+1):
+        ch=chr(a)
+        print(ch,end=" ")
+        a=a+1
+    print()
 # 1
 # 22
 # 333
 # 4444
-# n=int(input("enter no of rows:"))
-# for i in range(1,n+1):
-#     for j in range(i):
-#         print(i,end=" ")
-#     print()
+n=int(input("enter no of rows:"))
+for i in range(1,n+1):
+    for j in range(i):
+        print(i,end=" ")
+    print()
 # 1234
 # 123
 # 12
 # 1
-# n=int(input("enter no of rows:"))
-# for i in range(n):
-#     for j in range(1,n+1,1):
-#         print(j,end=" ")
-#     print() 
-#     n=n-1   
+n=int(input("enter no of rows:"))
+for i in range(n):
+    for j in range(1,n+1,1):
+        print(j,end=" ")
+    print() 
+    n=n-1   
 
     
 
