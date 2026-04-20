c5162106b733301c5ef9139dd7142caeed7ff92d
Pratibha Agrawal <agrawalpratibha63@gmail.com>
2026-04-20 12:46:25 +0530

    DAY25 PRACTICE

---
+++ Day25.py
@@ -0,0 +1,12 @@
+#############################################################################
+                              #Day24 Practice
+#############################################################################
+
+#QUESTION  Find the Integer Added to Array I
+nums1=eval(input("enter list of integers"))
+nums2=eval(input("enter list of integers"))
+n1=sorted(nums1)
+n2=sorted(nums2)
+res=n2[0]-n1[0]
+print(res)
+        
