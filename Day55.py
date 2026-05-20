34ca12bff668c86b4a4b1e8b854605e371c7504c
Pratibha Agrawal <agrawalpratibha63@gmail.com>
2026-05-20 18:43:09 +0530

    day55 practice

---
+++ Day55.py
@@ -0,0 +1,8 @@
+#############################################################################
+                              #Day52  Practice
+#############################################################################
+
+#Question  Find Minimum in Rotated Sorted Array II
+
+nums=eval(input("enter list of integers:"))
+print(min(nums))
