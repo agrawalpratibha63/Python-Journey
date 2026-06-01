5ab50dcfb41aab0e473a646bcf31a20b7d0a7075
Pratibha Agrawal <agrawalpratibha63@gmail.com>
2026-06-01 16:19:16 +0530

    day66 practice

---
+++ Day66.py
@@ -0,0 +1,10 @@
+#############################################################################
+                              #Day66  Practice
+#############################################################################
+   
+#Question   Chef in his Office
+
+T=int(input("enter :"))
+for i in range(T):
+    a,b=map(int,input("enter numbers").split()) 
+    print((a*4)+b)
