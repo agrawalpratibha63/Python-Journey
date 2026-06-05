8ba6b8064e6908a1a5f0939b2b211448449f659e
Pratibha Agrawal <agrawalpratibha63@gmail.com>
2026-06-05 21:06:10 +0530

    day69 practice

---
+++ Day69.py
@@ -0,0 +1,11 @@
+#############################################################################
+                              #Day67  Practice
+#############################################################################
+   
+#Question Count the Notebooks
+
+T=int(input("enter testcases"))
+for i in range(T):
+    x=int(input("enter integer:"))
+    p=(x*1000)//100
+    print(p)
